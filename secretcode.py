f4fe069db2f1f5592c4a6bf68d2abfc618631030
Sougandh4242 <sougandhbettad24@gmail.com>
2025-05-14 22:53:42 +0530

    Update secretcode.py

--- secretcode.py
+++ secretcode.py
@@ -1,38 +1,41 @@
 # coding and decoding of a secret word
 import random
-import os
 import string
-words=input("Enter message : ")
-length=len(words)
-option=input("Press 1 for Coding  \nPress 2 for Decoding \n ")
-if option=='1':
-    #coding
-    print("Your Secret code")
-    for word in words:
 
-        if (length>=3) : 
-            modified_word=word[1:]+word[0]
-            secret_first=''.join(random.choices(string.ascii_letters+string.digits,k=3))
-            secret_second=''.join(random.choices(string.ascii_letters+string.digits,k=3))
-            generated_word=secret_first+modified_word+secret_second
-            print(generated_word,end=" ")
+# Take input from the user
+words = input("Enter message : ").split()  # Split the message into individual words
+option = input("Press 1 for Coding  \nPress 2 for Decoding \n ")
 
-    else: print(word[::-1])
-elif option=='2':
-    #Decoding
+if option == '1':
+    # **Coding**
+    print("Your Secret code:")
     for word in words:
-       if length > 6:  # It should have at least 7 characters to be valid
+        length = len(word)  # Calculate length for each word individually
+
+        if length >= 3:
+            modified_word = word[1:] + word[0]
+            secret_first = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
+            secret_second = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
+            generated_word = secret_first + modified_word + secret_second
+            print(generated_word, end=" ")
+        else:
+            print(word[::-1], end=" ")
+
+elif option == '2':
+    # **Decoding**
+    print("Decoded message:")
+    for word in words:
+        length = len(word)  # Calculate length for each word individually
+        
+        if length > 6:  # It should have at least 7 characters to be valid
             # Remove the 3-character prefix and suffix
             new = word[3:-3]
 
-            # **Check if `new` is not empty** before trying to access
-            if len(new) > 0:
-                # Move the last character back to the start
-                decoded_word = new[-1] + new[:-1]
-            else:
-                decoded_word = new  # If it's empty, just leave it as is
-                
+            # Move the last character back to the start
+            decoded_word = new[-1] + new[:-1]
             print(decoded_word, end=" ")
-    else: print(word[::-1])
+        else:
+            print(word[::-1], end=" ")
 
-else:print("Invalid input")
+else:
+    print("Invalid input")
